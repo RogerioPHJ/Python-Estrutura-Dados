eb0e77dbeb538b0657a1e4dfbfd5290f681abb1b
RogerioPHJ <44878115+RogerioPHJ@users.noreply.github.com>
2020-02-19 00:19:17 -0300

    Gerado o inserir_elemento_final (como o nome já diz, insere o valor passado no final do vetor, e se o mesmo já estiver cheio, cria uma nova posição no vetor

--- main.py
+++ main.py
@@ -18,7 +18,16 @@ menu = int(input("Digite a opção desejada: "))
 
 if menu == 1:
     vetor_teste = vetor.Vetor(3)
-    vetor_teste.inserir_elemento_posicao(1, 0)
-    vetor_teste.inserir_elemento_posicao(3, 1)
+    #vetor_teste.inserir_elemento_posicao(1, 0)
+    #vetor_teste.inserir_elemento_posicao(3, 1)
+    vetor_teste.inserir_elemento_final(1)
+    vetor_teste.inserir_elemento_final(2)
+    vetor_teste.inserir_elemento_final(3)
+    vetor_teste.inserir_elemento_final(4) #Teste se gerou mais uma posição
+    vetor_teste.inserir_elemento_final(5) #Teste se gerou mais uma posição x2
     print(vetor_teste.listar_elemento(0))
     print(vetor_teste.listar_elemento(1))
+    print(vetor_teste.listar_elemento(2))
+    print(vetor_teste.listar_elemento(3)) #Impressão do teste
+    print(vetor_teste.listar_elemento(4)) #impressão do teste
+
--- vetores/vetor.py
+++ vetores/vetor.py
@@ -2,9 +2,16 @@ class Vetor():
     def __init__(self, tamanho):
         self.__tamanho = tamanho
         self.__elementos = [None] * tamanho
+        self.__posicao = 0
 
     def inserir_elemento_posicao(self, elemento, posicao):
         self.__elementos[posicao] = elemento
+
+    def inserir_elemento_final(self, elemento):
+        if self.__posicao >= len(self.__elementos): #Se o vetor já estiver cheio
+            self.__elementos = self.__elementos + [None] #Gera mais uma posição no Vetor,
+        self.__elementos[self.__posicao] = elemento
+        self.__posicao += 1
 
     def listar_elemento(self, posicao):
         return self.__elementos[posicao]
