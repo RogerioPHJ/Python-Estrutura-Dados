4d12969b8a2861cd82c59b84376069df3360d013
RogerioPHJ <44878115+RogerioPHJ@users.noreply.github.com>
2020-02-19 00:06:40 -0300

    Primeiro Commit, com Criação de Vetores ( criando um arquivo referente aos vetores)

---
+++ main.py
@@ -0,0 +1,24 @@
+from array import array
+from vetores import vetor
+
+#vetor_inteiros = array('b', [1,2,3])
+#print(vetor_inteiros)
+#vetor_inteiros.insert(3,4)
+#print(vetor_inteiros)
+#print(vetor_inteiros.index(2))
+
+print(30 * "-", "MENU", 30 * "-")
+print("1. vetores")
+print("2. Listas Ligadas")
+print("3. Listas Duplamente Ligadas")
+print("4. Pilhas")
+print("5. Filas")
+
+menu = int(input("Digite a opção desejada: "))
+
+if menu == 1:
+    vetor_teste = vetor.Vetor(3)
+    vetor_teste.inserir_elemento_posicao(1, 0)
+    vetor_teste.inserir_elemento_posicao(3, 1)
+    print(vetor_teste.listar_elemento(0))
+    print(vetor_teste.listar_elemento(1))
---
+++ vetores/vetor.py
@@ -0,0 +1,10 @@
+class Vetor():
+    def __init__(self, tamanho):
+        self.__tamanho = tamanho
+        self.__elementos = [None] * tamanho
+
+    def inserir_elemento_posicao(self, elemento, posicao):
+        self.__elementos[posicao] = elemento
+
+    def listar_elemento(self, posicao):
+        return self.__elementos[posicao]
