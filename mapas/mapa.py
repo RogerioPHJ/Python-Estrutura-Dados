1437a47abc8c09e6ec5293a842f979e92327e7e6
RogerioPHJ <44878115+RogerioPHJ@users.noreply.github.com>
2020-02-20 14:11:11 -0300

    Inserindo e Recuperando nos Mapas

--- mapas/mapa.py
+++ mapas/mapa.py
@@ -30,3 +30,23 @@ class Mapa():
                 categoria.remover_elemento(associacao)
                 return True
         return False
+    
+    def adicionar(self, chave, valor):
+        if self.contem_chave(chave):
+            self.remover(chave)
+        numero_espalhamento = self.gerar_numero_espalhamento(chave)
+        categoria = self.__elementos.recuperar_elemento_no(numero_espalhamento)
+        categoria.inserir(associacao.Associacao(chave, valor))
+
+    def recuperar(self, chave):
+        numero_espalhamento = self.gerar_numero_espalhamento(chave)
+        categoria = self.__elementos.recuperar_elemento_no(numero_espalhamento)
+        for i in range(categoria.tamanho):
+            associacao = categoria.recuperar_elemento_no(i)
+            if associacao.chave == chave:
+                return associacao.valor
+        return False
+
+    def __str__(self):
+        temp = self.__elementos.__str__()
+        return temp
