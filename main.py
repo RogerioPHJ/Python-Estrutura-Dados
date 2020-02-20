8942b22a52cc78a9cb18da68b794c3910acb690c
RogerioPHJ <44878115+RogerioPHJ@users.noreply.github.com>
2020-02-20 14:28:24 -0300

    Travessia em Ordem

--- main.py
+++ main.py
@@ -131,8 +131,7 @@ elif menu == 8:
     print(arvore_teste)
     arvore_teste.inserir_elemento(no_arvore_inteiro.NoArvoreInteiro(8))
     arvore_teste.inserir_elemento(no_arvore_inteiro.NoArvoreInteiro(7))
-    print(arvore_teste)
-    print(arvore_teste.buscar(no_arvore_inteiro.NoArvoreInteiro(6)))
+    print(arvore_teste.em_ordem())
 
 
 else:
